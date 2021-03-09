a992c17faa62f28e89fb034a25a903bf04a90d65
uromahn <ulrich.romahn@gmail.com>
2021-03-08 23:14:35 -0800

    Updated comment

--- pkg/util/listwatcher.go
+++ pkg/util/listwatcher.go
@@ -11,8 +11,12 @@ import (
 )
 
 // NewFilteredEndpointsListWatchFromClient creates a new ListWatch from the specified client, resource, namespace, and option modifier.
-// Option modifier is a function takes a ListOptions and modifies the consumed ListOptions. Provide customized modifier function
+// Option modifier is a function that takes a ListOptions and modifies the consumed ListOptions. Provide customized modifier function
 // to apply modification to ListOptions with a field selector, a label selector, or any other desired options.
+//
+// This function was copied over from the NewFilteredListWatchFromClient in https://github.com/kubernetes/client-go/blob/master/tools/cache/listwatch.go
+// and modified to use a v1.CoreV1Interface instead of a RESTClient. This required us to make the function specific for the Endoints type
+// instead of having it generic for all types. However, this function also works with the Fake client.
 func NewFilteredEndpointsListWatchFromClient(ctx context.Context, c v1.CoreV1Interface, namespace string, optionsModifier func(options *metav1.ListOptions)) *cache.ListWatch {
 	listFunc := func(options metav1.ListOptions) (runtime.Object, error) {
 		optionsModifier(&options)
